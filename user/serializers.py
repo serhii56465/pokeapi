768f63963ba00d222a500f7168e8bb6ec2fd2587
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-06-18 16:41:09 +0300

    UserUpdatePokemonSerializer only for updating  pokemons, UserListSerializer without pokemons updating

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "username", "email", "password", "is_staff", "pokemons")
-        read_only_fields = ("is_staff",)
+        read_only_fields = ("is_staff", "pokemons")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
@@ -43,17 +43,17 @@ class UserUpdatePokemonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff", "pokemons")
-        read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        fields = ("id", "username", "email", "is_staff", "pokemons")
+        read_only_fields = ("is_staff", "username", "email")
 
     def update(self, instance, validated_data):
         pokemons_data = validated_data.pop("pokemons", None)
+        user = super().update(instance, validated_data)
         for pokemon_ in pokemons_data:
             if pokemon_.user is None:
-                setattr(pokemon_, "user", instance)
+                setattr(pokemon_, "user", user)
                 pokemon_.save()
-            elif pokemon_.user == instance:
+            elif pokemon_.user == user:
                 setattr(pokemon_, "user", None)
                 pokemon_.save()
-        return instance
+        return user
